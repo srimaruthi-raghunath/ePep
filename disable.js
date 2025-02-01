a1fc3226955e04b8367458e4d457d6f20c5f05bf
srimaruthi-raghunath <raghunath.srimaruthi@gmail.com>
2025-02-01 18:04:52 +0530

    logo added in title and index comment style removed

---
+++ disable.js
@@ -0,0 +1,7 @@
+// disable-right-click.js
+// document.addEventListener('contextmenu', function(event) {
+//     event.preventDefault();
+//     alert('Right-click is disabled.');
+//   });
+ 
+//   <script src="disable-right-click.js"></script>
