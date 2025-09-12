d5afb421c0baf47428cf48bc6ffce0e6a7c3c91b
srimaruthi-raghunath <129269685+srimaruthi-raghunath@users.noreply.github.com>
2025-09-12 16:20:09 +0530

    removed some unnecessary comment and renamed the disaple.js to view.js
    removed some unnecessary comment and renamed the disaple.js to view.js

--- js/disable.js
+++
@@ -1,7 +0,0 @@
-// disable-right-click.js
-// document.addEventListener('contextmenu', function(event) {
-//     event.preventDefault();
-//     alert('Right-click is disabled.');
-//   });
- 
-//   <script src="disable-right-click.js"></script>
---
+++ js/view.js
@@ -0,0 +1,4 @@
+// document.addEventListener('contextmenu', function(e) {
+//   e.preventDefault();
+//   alert("Right-click is disabled on this site.");
+// });
